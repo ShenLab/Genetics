7d0edd789fb470f2cadbd80d4a33ef1ff6189ede
Josh Shen <joshuashen@gmail.com>
2014-06-23 22:43:13 -0400

    minor

--- utils/prepare_curation.rb
+++ utils/prepare_curation.rb
@@ -4,7 +4,7 @@ def main
   File.new(annovar, 'r').each do |line|
     cols = line.split(/\t/)
     chr,pos, gene, ref, alt,  aa, info, func = cols[0], cols[1], cols[6].gsub(/\"/, ""), cols[3], cols[4], cols[8], cols[-1], cols[7]
-    
+    next if pos == "Start"
     if gene =~ /^(\S+)\((\S+)\)/
       geneName = $1
       aachange = "splicing;#{$2}"
@@ -15,12 +15,13 @@ def main
     end
     
     infof = info.gsub(/\"/,"").split(';')
-    sid, ad = infof[0], infof[1]
+    sid, ad = infof[0], infof[1].split(',')[0,2]
 
     if func == ""
       func = "unknown"
     end
-    puts "#{sid}\thg19\t#{geneName}\t#{pos}\t#{ref}\t#{alt}\t0/1\t#{aachange}\tde_novo\tNA\t#{ad}\t#{func}"
+    func.gsub!(/\s+/, "_")
+    puts "#{sid}\thg19\t#{geneName}\t#{pos}\t#{ref}\t#{alt}\t0/1\t#{aachange}\tde_novo\tNA\t#{ad.join("\t")}\t#{func}"
   end
 end
 
