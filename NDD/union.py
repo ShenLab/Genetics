c767630e0885aa4ddc6c7aba74ba099c8ed4a68e
Alexander Hsieh <alexanderhsieh7@gmail.com>
2015-07-28 12:20:35 -0400

    updated union.py to take any number of arguments

--- NDD/union.py
+++ NDD/union.py
@@ -1,28 +1,17 @@
-## Usage: python union.py <list1> <list2> <list3> > <union list>
-## Purpose: union operation on 3 lists, removes duplicates
-## Output: union of 3 lists
+## Usage: python union.py list1.txt list2.txt .... > unionlist.txt
+## Purpose: union operation on a variable number of list files, removes duplicates
+## Output: union of all argument lists
 import sys
 
-f1 = open(sys.argv[1],'r')
-f2 = open(sys.argv[2],'r')
-f3 = open(sys.argv[3],'r')
+tmp = []
 
-l1 = []
-for line in f1:
-    l1.append(line.strip().split()[0])
-f1.close()
+for arg in sys.argv[1:]:
+	f = open(arg,'r')
+	for line in f:
+		tmp1 = line.strip().split()
+		tmp.append(tmp1[0])
+	f.close()
 
-l2 = []
-for line in f2:
-    l2.append(line.strip().split()[0])
-f2.close()
-
-l3 = []
-for line in f3:
-    l3.append(line.strip().split()[0])
-f3.close()
-
-full = l1+l2+l3
-uniq = list(set(full))
-for line in uniq:
-    print line.strip()
+uniq = list(set(tmp))
+for l in uniq:
+	print l
